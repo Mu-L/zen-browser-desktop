b4575de026125154fc76c20ac9b293ba1d3b73e0
mr. M <mauro.balades@tutanota.com>
2025-04-05 01:26:14 +0200

    Initial development for folders

--- src/browser/base/zen-components/ZenFolders.mjs
+++ src/browser/base/zen-components/ZenFolders.mjs
@@ -1,16 +1,27 @@
 {
-  class ZenFolders {
-    constructor() {
+  class ZenFolders extends ZenPreloadedFeature {
+    init() {
+      this.#initContextMenu();
       this.#initEventListeners();
     }
 
     #initEventListeners() {
-      document.addEventListener('TabGrouped', this.#onTabGrouped.bind(this));
-      document.addEventListener('TabUngrouped', this.#onTabUngrouped.bind(this));
-      document.addEventListener('TabGroupRemoved', this.#onTabGroupRemoved.bind(this));
-      document.addEventListener('TabGroupCreate', this.#onTabGroupCreate.bind(this));
-      document.addEventListener('TabPinned', this.#onTabPinned.bind(this));
-      document.addEventListener('TabUnpinned', this.#onTabUnpinned.bind(this));
+      window.addEventListener('TabGrouped', this.#onTabGrouped.bind(this));
+      window.addEventListener('TabUngrouped', this.#onTabUngrouped.bind(this));
+      window.addEventListener('TabGroupRemoved', this.#onTabGroupRemoved.bind(this));
+      window.addEventListener('TabGroupCreate', this.#onTabGroupCreate.bind(this));
+      window.addEventListener('TabPinned', this.#onTabPinned.bind(this));
+      window.addEventListener('TabUnpinned', this.#onTabUnpinned.bind(this));
+      window.addEventListener('TabGroupExpand', this.#onTabGroupExpand.bind(this));
+      window.addEventListener('TabGroupCollapse', this.#onTabGroupCollapse.bind(this));
+      document.getElementById('zen-context-menu-new-folder').addEventListener('command', this.#onNewFolder.bind(this));
+    }
+
+    #initContextMenu() {
+      const contextMenuItems = window.MozXULElement.parseXULToFragment(`
+        <menuitem id="zen-context-menu-new-folder" data-l10n-id="zen-toolbar-context-new-folder"/>
+      `);
+      document.getElementById('toolbarNavigatorItemsMenuSeparator').before(contextMenuItems);
     }
 
     #onTabGrouped(event) {
@@ -113,6 +124,61 @@
         return true;
       }
       return this._piningFolder;
+    }
+
+    #onNewFolder(event) {
+      const group = gBrowser.addTabGroup([gBrowser.selectedTab], {
+        insertBefore: ZenWorkspaces.pinnedTabsContainer.querySelector('.vertical-pinned-tabs-container-separator'),
+        label: 'New Folder',
+      });
+      group.pinned = true;
+    }
+
+    #onTabGroupExpand(event) {}
+
+    async #onTabGroupCollapse(event) {
+      const group = event.target;
+      const groupRect = group.getBoundingClientRect();
+      const tabsContainer = group.querySelector('.tab-group-container');
+      const animations = [];
+      let selectedTabHeight = 0;
+      for (const item of group.querySelector('.tab-group-container').children) {
+        if (item.hasAttribute('visuallyselected')) {
+          selectedTabHeight = item.getBoundingClientRect().height;
+          continue;
+        }
+        const rect = item.getBoundingClientRect();
+        // Calculate distance between the group label and the tab in order to animate
+        // the tab translating into it vertically
+        const translateY = groupRect.top - rect.top;
+        animations.push(
+          gZenUIManager.motion.animate(
+            item,
+            {
+              y: [0, translateY],
+            },
+            {
+              duration: 0.2,
+              type: 'spring',
+              bounce: 0,
+            }
+          )
+        );
+      }
+      animations.push(
+        gZenUIManager.motion.animate(
+          tabsContainer,
+          {
+            height: [groupRect.height, selectedTabHeight],
+          },
+          {
+            duration: 0.2,
+            type: 'spring',
+            bounce: 0,
+          }
+        )
+      );
+      await Promise.all(animations);
     }
   }
 
